4e7f49fec34d8bd44007e185485375ceab2c532e
Pochere <pochere2008@gmail.com>
2025-09-06 23:11:13 +0300

    Fix: implement get_librarian_for_library with Librarian.objects.get(library=...)

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -15,4 +15,4 @@ def get_books_in_library(library_name):
 # 3. Retrieve the librarian for a library
 def get_librarian_for_library(library_name):
     library = Library.objects.get(name=library_name)
-    return library.librarian  # OneToOne relationship
+    return Librarian.objects.get(library=library)  # OneToOne relationship
