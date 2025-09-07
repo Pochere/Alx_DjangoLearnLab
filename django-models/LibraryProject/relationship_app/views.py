7c7d20f6abf861a6db353eeec1de5f7ce41e8228
Pochere <pochere2008@gmail.com>
2025-09-07 19:27:13 +0300

    Implemented list_books and LibraryDetailView with templates and urls

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.views.generic import DetailView
 from .models import Book, Library
 
@@ -12,3 +12,4 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
+
