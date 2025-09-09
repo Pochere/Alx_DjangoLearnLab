d433b11c38b8163d6b648e1a1e5479e041ca37c2
Pochere <pochere2008@gmail.com>
2025-09-09 09:50:06 +0300

    addeda missing / in add_book

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
     path("librarian-view/", librarian_view, name="librarian_view"),   # Librarian-only page
     path("member-view/", member_view, name="member_view"),     # Member-only page
 
-    path("add_book", views.add_book, name="add_book"),
+    path("add_book/", views.add_book, name="add_book"),
     path("edit_book/<int:book_id>/", views.edit_book, name="edit_book"),
     path("delete_book/<int:book_id>/", views.delete_book, name="delete_book"),
 ]
