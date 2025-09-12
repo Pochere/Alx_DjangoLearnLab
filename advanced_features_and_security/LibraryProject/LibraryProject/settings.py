fcf9e19f5d7ade09dcf9075dce621f7c581dc877
Pochere <pochere2008@gmail.com>
2025-09-12 21:34:29 +0300

    Implement secure view using ORM filtering in book_list with comments for SQL injection protection

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -124,3 +124,17 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "bookshelf.CustomUser"
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False   # only True when actively developing locally
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']  # add your domain/IP in production
+
+# Browser-side protections
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+X_FRAME_OPTIONS = "DENY"
+
+# Enforce HTTPS-only cookies (useful when deployed with HTTPS)
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
