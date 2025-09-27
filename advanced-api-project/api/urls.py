d0132aebfc90874c4a0fd19ae90d2d42bfa07867
Pochere <pochere2008@gmail.com>
2025-09-27 18:35:03 +0300

    Fixed URL patterns for books/update and books/delete, added DRF permissions, included api urls in project-level urls.py

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     path('books/', BookListView.as_view(), name='book-list'),
     path('books/create/', BookCreateView.as_view(), name='book-create'),     # POST create
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),   # GET detail
-    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'), # PUT/PATCH
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'), # DELETE
+    path('books/update/', BookUpdateView.as_view(), name='book-update'), # PUT/PATCH
+    path('books/delete/', BookDeleteView.as_view(), name='book-delete'), # DELETE
 ]
